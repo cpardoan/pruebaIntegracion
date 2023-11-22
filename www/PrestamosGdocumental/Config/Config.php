802c37cae15fd5c082a205613e25a9d5c30d8593
cpardoan <142365655+cpardoan@users.noreply.github.com>
2023-11-22 14:52:42 -0500

    Add files via upload

---
+++ www/PrestamosGdocumental/Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://localhost:8080/PrestamosGdocumental/";
+const host = "localhost";
+const user = "root";
+const pass = "rootpass";
+const db = "prestamos";
+const charset = "charset=utf8";
+?>
